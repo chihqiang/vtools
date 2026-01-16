bdb5a847ff72d246e4a49ba3b93f0dd66f2fc66e
zhiqiang <zhiqiang2033@gmail.com>
2026-01-16 15:17:42 +0800

    refactor: 将路由历史模式从 web history 改为 hash 模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import TimestampTool from '@/views/tools/TimestampTool.vue'
 import JsonFormatter from '@/views/tools/JsonFormatter.vue'
@@ -20,7 +20,7 @@ type RouteMeta = {
 }
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
